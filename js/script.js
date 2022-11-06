b52276181e905a8f9d18a4bc117bf10cdde17274
Kiwac <kkiwac@gmail.com>
2022-11-06 17:00:09 +0300

    New slider

--- js/script.js
+++ js/script.js
@@ -2,6 +2,35 @@ $(document).ready(function(){
   $('.slider').slick({
     arrows: true,
     slidesToShow: 5,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    speed: 1000,
+    touchThreshold: 20,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow:4,
+        }
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+      {
+        breakpoint: 855,
+        settings: {
+          slidesToShow:2,
+        }
+      },
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow:1,
+        }
+      }
+    ]
   });
 });
